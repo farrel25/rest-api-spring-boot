b21cb6090a3ef326a6722980d9d70c058c1d3516
Farrel Putra <frl25ap@gmail.com>
2022-10-02 01:02:08 +0700

    configure swagger UI

--- src/main/java/practice/restapispringboot/swagger/SwaggerConfig.java
+++ src/main/java/practice/restapispringboot/swagger/SwaggerConfig.java
@@ -1,13 +1,25 @@
 package practice.restapispringboot.swagger;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * To use Swagger UI, we need to add an additional Maven dependency:
+ * springfox-swagger-ui
+ * 
+ * After that, we can test it in our browser by visiting:
+ * @link http://localhost:8080/swagger-ui/
+ */
 
 @Configuration
 @EnableSwagger2
@@ -15,10 +27,30 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        // return new Docket(DocumentationType.SWAGGER_2)
+        // .select()
+        // .apis(RequestHandlerSelectors.any())
+        // .paths(PathSelectors.any())
+        // .build();
         return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.any())
+        .apis(RequestHandlerSelectors.basePackage("practice.restapispringboot.controllers"))
         .paths(PathSelectors.any())
-        .build();
+        .build()
+        .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+            "REST API Spring Boot Documentation",
+            "This is the documentaion of rest api with springboot",
+            "v1.0",
+            "Terms of Service",
+            new Contact("Farrel Putra", "restapispringboot.com", "f.athaillahp@gmail.com"),
+            "Apache License",
+            "www.apache.com",
+            Collections.emptyList()
+            );
+        return apiInfo;
     }
 }
